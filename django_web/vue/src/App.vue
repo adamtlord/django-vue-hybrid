1e5e6e07171ebe9a60e8a27473628026fe870859
Adam Lord <adam@tivix.com>
2021-03-01 17:59:40 -0800

    Add Tailwind css with example structure

--- django_web/vue/src/App.vue
+++ django_web/vue/src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="font-sans text-center text-gray-700 mx-8">
+    <nav class="py-8 font-bold">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- django_web/vue/src/views/Home.vue
+++ django_web/vue/src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home mb-12">
+    <img alt="Vue logo" src="../assets/logo.png" class="mx-auto" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
