80a8a15feb03c3f9a858a40a872c07ab18b6cdbe
Adam Lord <adam@tivix.com>
2021-05-06 11:34:10 -0700

    feat: add link to Django example page, better router link styling

--- django_web/vue/src/App.vue
+++ django_web/vue/src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div id="app" class="font-sans text-center text-gray-700 mx-8">
     <nav class="py-8 font-bold">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link active-class="text-green-500" to="/">Home</router-link> |
+      <router-link active-class="text-green-500" to="/about">About</router-link> |
+      <a href="/example/">Django Page</a>
     </nav>
     <router-view />
   </div>
 </template>
-
-<style>
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
